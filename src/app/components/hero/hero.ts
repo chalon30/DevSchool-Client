8c6cab4966799ab77071cd28d6da886c1b56bd6c
chalon30 <angelfabiano3030@gmail.com>
2025-11-15 11:09:19 -0500

    hero v2

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
